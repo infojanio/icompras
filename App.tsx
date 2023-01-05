51cfdd5e487fd3df45f6d32436aaa8eba56c662a
JÂNIO FLORÊNCIO DOS SANTOS <info.janio@gmail.com>
2023-01-05 01:41:37 +0000

    initial

--- App.tsx
+++ App.tsx
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>app mercado!</Text>
+      <Text>app mercado cb!</Text>
       <StatusBar style="auto" />
     </View>
   );
