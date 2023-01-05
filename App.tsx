e2d23208694fa7fe9946eadf083483982f9b7b10
JÂNIO FLORÊNCIO DOS SANTOS <info.janio@gmail.com>
2023-01-05 20:01:26 +0000

    initial

--- App.tsx
+++ App.tsx
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Iniciando projeto apps mercados!</Text>
+      <Text>Iniciando projeto apps mercados online!</Text>
       <StatusBar style="auto" />
     </View>
   );
