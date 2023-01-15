24259ee4db8089fcdc55fecfb23804d90f978cd4
Janio Florencio dos Santos <info.janio@gmail.com>
2023-01-15 16:11:38 -0300

    localization in maps

--- src/components/Map/index.tsx
+++ src/components/Map/index.tsx
@@ -1,29 +1,141 @@
-import React, { useState, useEffect } from 'react';
-import MapView from 'react-native-maps';
-import { StyleSheet, View, Dimensions } from 'react-native';
+import React, { useState, useEffect, useRef } from 'react'
+import MapView, { Marker } from 'react-native-maps'
+import {
+  StyleSheet,
+  View,
+  Dimensions,
+  Platform,
+  PermissionsAndroid,
+} from 'react-native'
 
+//import Geolocation from '@react-native-community/geolocation'
+import * as Location from 'expo-location'
 
+const { width, height } = Dimensions.get('screen')
 
+/* 
+fazer a tipagem
+interface Props {
+  latitude: number
+  longitude: number
+}
 
+type region = LocationObject 
+ */
 export function Map() {
-   
+  const [region, setRegion] = useState()
+  const [markers, setMarkers] = useState([]) //
+  const mapRef = useRef<MapView>(null)
+
+  useEffect(() => {
+    handleMyPermission()
+  }, [])
+
+  //pega a permissão em projetos EXPO
+  function handleMyPermission() {
+    ;(async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync()
+      if (status !== 'granted') {
+        //setErrorMsg('Permission to access location was denied')
+        return
+      }
+
+      let region = await Location.getCurrentPositionAsync({})
+
+      setRegion(region)
+      console.log(region)
+    })()
+  }
+
+  //Mostra o ponto atual do usuário no mapa
+  function PointActual() {
+    const mapRef = useRef<MapView>(null)
+  }
+
+  //cria um marcador no mapa
+  function newMarker(e) {
+    //console.log(e.nativeEvent.coordinate.latitude)
+    //console.log(e.nativeEvent.coordinate.longitude)
+
+    let dados = {
+      key: markers.length, //cria chave aleatória, tamanho da lista
+      coords: {
+        latitude: e.nativeEvent.coordinate.latitude,
+        longitude: e.nativeEvent.coordinate.longitude,
+      },
+      pinColor: '#FF0000',
+    }
+
+    //garante o deslocamento para o local onde foi criado o marcador
+    setRegion({
+      latitude: e.nativeEvent.coordinate.latitude,
+      longitude: e.nativeEvent.coordinate.longitude,
+      latitudeDelta: 0.0922,
+      longitudeDelta: 0.0421,
+    })
+
+    //pega todos os marcadores e adiciona +1 na lista
+    setMarkers((oldArray) => [...oldArray, dados])
+  }
 
   return (
     <View style={styles.container}>
-      <MapView style={styles.map} />
+      <MapView
+        onMapReady={() => {
+          Platform.OS === 'android'
+            ? PermissionsAndroid.request(
+                PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+              ).then(() => {
+                console.log('Usuário aceitou')
+              })
+            : console.log('Usuário não aceitou')
+        }}
+        style={styles.map}
+        region={region}
+        ref={mapRef}
+        zoomEnabled={true}
+        minZoomLevel={15}
+        showsUserLocation={true}
+        loadingEnabled={true}
+        onPress={(e) => newMarker(e)}
+      >
+        {markers.map((marker) => {
+          return (
+            <Marker
+              key={marker.key}
+              coordinate={marker.coords}
+              pinColor={marker.pinColor}
+            />
+          )
+        })}
+      </MapView>
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center"
-    
+    alignItems: 'center',
   },
   map: {
-    width: '80%',
-    height: '60%',
+    width: width,
+    height: height,
   },
-});
+})
+
+/* Pega a localização em projetos CLI  
+function handleMyLocation() {
+    Geolocation.getCurrentPosition((info) => {
+      console.log('LAT', info.coords.latitude)
+      console.log('LONG', info.coords.longitude)
+
+      
+      setRegion({
+        latitude: info.coords.latitude,
+        longitude: info.coords.longitude,
+        latitudeDelta: 0.0922,
+        longitudeDelta: 0.0421,
+      })
+
+ */
--- src/screens/Localization.tsx
+++ src/screens/Localization.tsx
@@ -2,14 +2,14 @@ import { View, StyleSheet } from 'react-native'
 import { Center } from 'native-base'
 import React from 'react'
 import { Map } from '@components/Map'
-import { background } from 'native-base/lib/typescript/theme/styled-system'
+import { Input } from '@components/Input'
 
 export function Localization() {
   return (
     <View>
-      <Center marginTop={10} >Localization</Center>
+      <Input placeholder="Busque pelo CEP" />
       <View style={styles.container}>
-      <Map />
+        <Map />
       </View>
     </View>
   )
@@ -18,9 +18,8 @@ export function Localization() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    background: "#7fb9db"
-    
-  }
+    justifyContent: 'center',
+    alignItems: 'center',
+    background: '#7fb9db',
+  },
 })
